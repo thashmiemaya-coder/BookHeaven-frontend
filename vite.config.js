1d628ebad6a0f3a07cb7833f18b464a5b776144a
Thashmi Fernando <thashmiemaya@gmail.com>
2026-08-04 22:19:13 +0530

    Remove manual chunks causing runtime error

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 5173,
     proxy: {
-      '/api': { target: 'http://localhost:5000', changeOrigin: true },
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          redux: ['@reduxjs/toolkit', 'react-redux'],
-        },
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
       },
     },
   },
